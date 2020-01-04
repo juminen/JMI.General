58e458b8c6d85af92d15c5756eb6bfdec7256c36
juminen <jukkamietti@gmail.com>
2020-01-04 13:12:05 +0200

    Added namespace .VM.CED

---
+++ JMI.General.VM/CED/CreateNewItemViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JMI.General.VM.CED
+{
+    public abstract class CreateNewItemViewModel
+    {
+        #region constructors
+        #endregion
+
+        #region properties
+        protected abstract bool CreateEnabled { get; }
+        protected abstract string Title { get; }
+        #endregion
+
+        #region commands
+        private RelayCommand createCommand;
+        public RelayCommand CreateCommand
+        {
+            get
+            {
+                if (createCommand == null)
+                {
+                    createCommand = new RelayCommand(
+                        async param => await CreateItemAsync(),
+                        param => CreateEnabled);
+                }
+                return createCommand;
+            }
+        }
+
+        private RelayCommand cancelCommand;
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                if (cancelCommand == null)
+                {
+                    cancelCommand = new RelayCommand(
+                        param => Cancel(), 
+                        param => true);
+                }
+                return cancelCommand;
+            }
+        }
+        #endregion
+
+        #region methods
+        protected abstract Task CreateItemAsync();
+
+        public void Cancel()
+        {
+            CreateCanceled?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected void SendItemCreatedEvent()
+        {
+            ItemCreated?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region events
+        public event EventHandler CreateCanceled;
+        public event EventHandler ItemCreated;
+        #endregion
+
+        #region event handlers
+        #endregion
+    }
+}
---
+++ JMI.General.VM/CED/DeleteItemViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JMI.General.VM.CED
+{
+    public abstract class DeleteItemViewModel
+    {
+        #region constructors
+        #endregion
+
+        #region properties
+        protected abstract string Title { get; }
+        #endregion
+
+        #region commands
+        private RelayCommand yesCommand;
+        public RelayCommand YesCommand
+        {
+            get
+            {
+                if (yesCommand == null)
+                {
+                    yesCommand = new RelayCommand(
+                        async param => await DeleteItemAsync(), 
+                        param => true);
+                }
+                return yesCommand;
+            }
+        }
+
+        private RelayCommand noCommand;
+        public RelayCommand NoCommand
+        {
+            get
+            {
+                if (noCommand == null)
+                {
+                    noCommand = new RelayCommand(
+                        param => Cancel(), 
+                        param => true);
+                }
+                return noCommand;
+            }
+        }
+        #endregion
+
+        #region methods
+        protected abstract Task DeleteItemAsync();
+
+        public void Cancel()
+        {
+            DeletionCanceled?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region events
+        public event EventHandler DeletionCanceled;
+        public event EventHandler ItemDeleted;
+        #endregion
+    }
+}
