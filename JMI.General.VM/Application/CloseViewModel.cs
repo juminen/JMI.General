209d5c0784e99648cb795c31e40757a8b63ee854
juminen <jukkamietti@gmail.com>
2020-01-19 17:33:52 +0200

    Added CloseViewModel and TabViewModel

---
+++ JMI.General.VM/Application/CloseViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JMI.General.VM.Application
+{
+    /// <summary>
+    /// Class for user interface items that are closable (eg. tab, editing form etc)
+    /// </summary>
+    public abstract class CloseViewModel : ObservableObject, IDisposable
+    {
+        #region constructors
+        #endregion
+
+        #region properties
+        private bool allowClose;
+        /// <summary>
+        /// Allows closing item.
+        /// </summary>
+        public bool AllowClose
+        {
+            get { return allowClose; }
+            protected set { SetProperty(ref allowClose, value); }
+        }
+        #endregion
+
+        #region commands
+        private RelayCommand closeCommand;
+        /// <summary>
+        /// Command is enabled by <see cref="AllowClose"/> and calls method <see cref="CloseRequested"/>
+        /// </summary>
+        public RelayCommand CloseCommand
+        {
+            get
+            {
+                if (closeCommand == null)
+                {
+                    closeCommand =
+                      new RelayCommand(
+                          async param => await RequestClose(),
+                          param => AllowClose);
+                }
+                return closeCommand;
+            }
+        }
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Does not do anything, override method in derived classes if neeeded.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            //Do nothing
+        }
+
+        /// <summary>
+        /// Calls <see cref="Dispose"/> and sends event <see cref="CloseRequested"/>
+        /// </summary>
+        protected virtual async Task RequestClose()
+        {
+            Dispose();
+            CloseRequested?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region events
+        /// <summary>
+        /// Event is send when close is requested, <see cref="RequestClose"/>
+        /// </summary>
+        public event EventHandler CloseRequested;
+        #endregion
+
+        #region event handlers
+        #endregion
+
+    }
+}
---
+++ JMI.General.VM/Application/TabViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace JMI.General.VM.Application
+{
+    /// <summary>
+    /// Class for tabs used in user interface
+    /// </summary>
+    public class TabViewModel<T> : CloseViewModel where T : class
+    {
+        #region constructors
+        #endregion
+
+        #region properties
+        private string displayText;
+        /// <summary>
+        /// Text displayed in tab header
+        /// </summary>
+        public string DisplayText
+        {
+            get { return displayText; }
+            set { SetProperty(ref displayText, value); }
+        }
+
+        private T tabContent;
+        /// <summary>
+        /// Content for the tab (usually a viewmodel class)
+        /// </summary>
+        public T TabContent
+        {
+            get { return tabContent; }
+            set { SetProperty(ref tabContent, value); }
+        }
+
+        #endregion
+
+        #region commands
+        #endregion
+
+        #region methods
+        #endregion
+
+        #region events
+
+        #endregion
+
+        #region event handlers
+        #endregion
+    }
+}
