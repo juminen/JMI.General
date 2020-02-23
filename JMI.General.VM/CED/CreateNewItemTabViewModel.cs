3b9c41bdc68b01a7bfe7136bbfd79a0c7498027c
juminen <jukkamietti@gmail.com>
2020-02-23 06:12:27 +0200

    New TabViewModels for Log and CreteNewItem

---
+++ JMI.General.VM/CED/CreateNewItemTabViewModel.cs
@@ -0,0 +1,109 @@
+﻿using JMI.General.VM.Application;
+using System;
+using System.Threading.Tasks;
+
+namespace JMI.General.VM.CED
+{
+    /// <summary>
+    /// Base class for views aimed to create new items in tab.
+    /// </summary>
+    public abstract class CreateNewItemTabViewModel : TabViewModel
+    {
+        #region constructors
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// Enables/disables <see cref="CreateCommand"/>
+        /// </summary>
+        protected abstract bool CreateEnabled { get; }
+
+        private string title;
+        /// <summary>
+        /// For view title/header
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            protected set { SetProperty(ref title, value); DisplayText = title; }
+        }
+        #endregion
+
+        #region commands
+        private RelayCommand createCommand;
+        /// <summary>
+        /// Calls method <see cref="CreateItemAsync"/>. Command is enabled disabled by boolean <see cref="CreateEnabled"/>.
+        /// </summary>
+        public RelayCommand CreateCommand
+        {
+            get
+            {
+                if (createCommand == null)
+                {
+                    createCommand = new RelayCommand(
+                        async param => await CreateItemAsync(),
+                        param => CreateEnabled);
+                }
+                return createCommand;
+            }
+        }
+
+        private RelayCommand cancelCommand;
+        /// <summary>
+        /// Calls method <see cref="Cancel"/>. Command is always enabled.
+        /// </summary>
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                if (cancelCommand == null)
+                {
+                    cancelCommand = new RelayCommand(
+                        param => Cancel(),
+                        param => true);
+                }
+                return cancelCommand;
+            }
+        }
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Absract methor for derived classes.
+        /// </summary>
+        protected abstract Task CreateItemAsync();
+
+        /// <summary>
+        /// Sends event <see cref="CreateCanceled"/>
+        /// and calls <see cref="RequestCloseViewModel.RequestClose"/>.
+        /// </summary>
+        public virtual void Cancel()
+        {
+            CreateCanceled?.Invoke(this, EventArgs.Empty);
+            RequestClose();
+        }
+
+        /// <summary>
+        /// Sends event <see cref="ItemCreated"/>
+        /// </summary>
+        protected void SendItemCreatedEvent()
+        {
+            ItemCreated?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region events
+        /// <summary>
+        /// Event is fired when creation is canceled (method <see cref="Cancel"/>).
+        /// </summary>
+        public event EventHandler CreateCanceled;
+        /// <summary>
+        /// Event is fired when item is created succesfully. Use <see cref="SendItemCreatedEvent"/> to send event.
+        /// </summary>
+        public event EventHandler ItemCreated;
+        #endregion
+
+        #region event handlers
+        #endregion
+    }
+}
---
+++ JMI.General.VM/Logging/LogTabViewModel.cs
@@ -0,0 +1,111 @@
+﻿using JMI.General.Logging;
+using JMI.General.VM.Application;
+using JMI.General.VM.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace JMI.General.VM.Logging
+{
+    public class LogTabViewModel : TabViewModel
+    {
+        #region constructors
+        public LogTabViewModel(Logger logger)
+        {
+
+            logSystem = logger ?? throw new ArgumentNullException(nameof(logger) + " can not be null");
+            messages = new ObservableCollection<LogMessageViewModel>();
+            Messages = new ListCollectionView(messages)
+            {
+                IsLiveSorting = true
+            };
+            Messages.LiveSortingProperties.Add(nameof(LogMessageViewModel.Time));
+            Messages.SortDescriptions.Add(new SortDescription(nameof(LogMessageViewModel.Time), ListSortDirection.Ascending));
+            logSystem.MessageReceived += OnLogMessageReceived;
+            logSystem.MessagesCleared += OnLogMessagesCleared;
+            CreateCommands();
+            LogTitle = logSystem.Name;
+            AllowClose = true;
+        }
+        #endregion
+
+        #region properties
+        protected readonly Logger logSystem;
+        protected ObservableCollection<LogMessageViewModel> messages;
+        public ListCollectionView Messages { get; private set; }
+        public ReadOnlyCollection<CommandGroupViewModel> CommandGroups { get; private set; }
+
+        private string logTitle;
+        /// <summary>
+        /// Title shown in log header. Default title is "Log". 
+        /// <see cref="TabViewModel.DisplayText"/> is same as Title.
+        /// </summary>
+        public string LogTitle
+        {
+            get { return logTitle; }
+            set { SetProperty(ref logTitle, value); DisplayText = logTitle; }
+        }
+
+        #endregion
+
+        #region commands
+        #endregion
+
+        #region methods
+        public override void Dispose()
+        {
+            messages.Clear();
+            logSystem.MessageReceived -= OnLogMessageReceived;
+            logSystem.MessagesCleared -= OnLogMessagesCleared;
+        }
+
+        private void CreateCommands()
+        {
+            List<CommandViewModel> list = new List<CommandViewModel>();
+
+            RelayCommand clearRelay =
+                new RelayCommand(
+                    param => logSystem.ClearMessages(),
+                    param => logSystem.Messages.Count > 0);
+            CommandViewModel clearCvm =
+                new CommandViewModel("Clear log", clearRelay);
+            list.Add(clearCvm);
+
+            RelayCommand copyClipBoardRelay =
+                new RelayCommand(
+                    param => logSystem.CopyToClipboard(),
+                    param => logSystem.Messages.Count > 0);
+            CommandViewModel copyClipboardCvm =
+                new CommandViewModel("Copy to clipboard", copyClipBoardRelay);
+            list.Add(copyClipboardCvm);
+
+            List<CommandGroupViewModel> commandGroupsList = new List<CommandGroupViewModel>();
+            CommandGroupViewModel group = new CommandGroupViewModel("Actions");
+            foreach (CommandViewModel item in list)
+            {
+                group.Commands.Add(item);
+            }
+            commandGroupsList.Add(group);
+
+            CommandGroups = new ReadOnlyCollection<CommandGroupViewModel>(commandGroupsList);
+        }
+        #endregion
+
+        #region events
+        #endregion
+
+        #region event handlers
+        private void OnLogMessagesCleared(object sender, EventArgs e)
+        {
+            messages.Clear();
+        }
+
+        private void OnLogMessageReceived(object sender, LogMessageEventArgs e)
+        {
+            messages.Add(new LogMessageViewModel(e.Message, logSystem.TimeFormat));
+        }
+        #endregion
+    }
+}
