e20a0386cb98fd6918cadce9d34956c1145866b2
juminen <jukkamietti@gmail.com>
2020-05-10 07:35:24 +0300

    Added namespace Identifiers

---
+++ JMI.General/Identifiers/IIdentifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JMI.General.Identifiers
+{
+    /// <summary>
+    /// Provides mechanism for identifying objects
+    /// </summary>
+    public interface IIdentifier
+    {
+        /// <summary>
+        /// Returns the unique (<see cref="Guid"/>) id of the item as a string.
+        /// </summary>
+        string Id { get; }
+        /// <summary>
+        /// Returns the unique id of the item.
+        /// </summary>
+        /// <returns><see cref="Guid"/></returns>
+        Guid GetGuid();
+    }
+}
