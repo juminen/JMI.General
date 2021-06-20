04707388b83b6548ce992e87c861502dac830cff
juminen <jukkamietti@gmail.com>
2021-06-20 09:03:37 +0300

    Created new namespace JMI.General.View.WPF.Converters and Added converters InverseBooleanConverter and LevelToLeftMarginConverter.

---
+++ JMI.General.View.WPF/Converters/LevelToLeftMarginConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JMI.General.View.WPF.Converters
+{
+    /// <summary>
+    /// Converts intendation to thickness that can be used in margins.
+    /// Value is intendation level (type int), and parameter is intendation length (type double).
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class LevelToLeftMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int && parameter is double)
+            {
+                int level = (int)value;
+                double width = (double)parameter;
+                double margin = level * width;
+                return new Thickness(margin, 0, 0, 0);
+            }
+            return new Thickness();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
